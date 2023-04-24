fde6066b3fd6ab36e06e9833b60b4b1f513bc98d
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-25 01:29:41 +0300

    add change baclgroundColor & timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('button[data-start]')
+const buttonStop = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
+let timerId
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function clickStart(event) {
+    timerId = setInterval(() => body.style.backgroundColor = getRandomHexColor(), 1000)
+    
+    buttonStart.disabled = true
+}
+buttonStart.addEventListener('click', clickStart)
+
+function clickStop(event) {
+    clearInterval(timerId)
+    
+    buttonStart.disabled = false
+}
+buttonStop.addEventListener('click', clickStop)
 
