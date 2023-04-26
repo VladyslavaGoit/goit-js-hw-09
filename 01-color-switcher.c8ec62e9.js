bf07491130056d54bc7fb79597001655a8c073fd
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-04-26 08:32:30 +0000

    Deploying to gh-pages from @ VladyslavaGoit/goit-js-hw-09@d63620a9eb9981f36cf9e7642be3bdd9c523ac81 🚀

--- 01-color-switcher.c8ec62e9.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n;t.addEventListener("click",(function(e){n=setInterval((()=>o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`),1e3),t.disabled=!0})),e.addEventListener("click",(function(e){clearInterval(n),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.c8ec62e9.js.map
